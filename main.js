5da0ea87c40eaa6fc190a24427d7040a4ebfa202
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-01-17 20:52:46 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,101 @@
+
+// Homework 2
+
+// Simple version:
+// let UAH = 27;
+//
+// for (let Dolar = 1; Dolar <= 100; Dolar = Dolar + 1 ) {
+//     console.log(`sqrt ${Dolar} = ${Dolar * UAH}`);
+// }
+
+
+
+// Hard version
+// let UAH = 27;
+// let userValue = prompt("Enter how many dollars you want to buy? ");
+//
+// for (let Dolar = 1; Dolar <= userValue; Dolar = Dolar + 1 ) {
+//     console.log(`If you want to buy ${Dolar} = it cost in UAH: ${Dolar * UAH}`);
+// }
+
+
+
+// Homework 1
+// for (let i = 20; i <= 30; i = i + 1) {
+//     console.log(i)
+//     console.log(i + 0.5)
+// }
+
+
+// Homework 3
+// let x = '';
+// for (i = 1; i <= 100; i = i + 1){
+//
+//     console.log(`sqrt ${i} = ${i * i}`);
+//
+// }
+
+
+
+
+
+// Homework 4
+// function prime(num){
+//     if(num == 1) return "The number is divided by itself and by 1";
+//     let t = num / 2;
+//     let k = 2;
+//     while(k <= t) {
+//         if(num % k == 0) {
+//             return "Number is not divided by itself and by 1";
+//         } else {
+//             k++;
+//         }
+//     }
+//     return "The number is divided by itself and by 1";
+// }
+//
+// console.log(prime(prompt("Enter your number ")))
+
+
+
+
+
+
+
+
+
+
+
+// Homework 5
+// let b = prompt("Enter your number: ");
+//
+//     let sum = '';
+//
+// for (let i = 3; i <= 3; i = i + 1) {
+//     sum = sum + i;
+// }
+//
+//
+//     // computing power
+//     let p = Math.log10(b)/ Math.log10(sum);
+//
+//     // checking to see if power is an integer or not
+//     if(p - Math.floor(p) == 0) {
+//         console.log(`можна одержати це число ${b} якщо помножити число: ${sum} на себе: ${p} разів (раз)`);
+//     }
+//     else
+//         console.log("NO");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
